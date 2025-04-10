cec070b4f9171ced8abd027764cdf775c8935627
hato72 <hatotori72@gmail.com>
2025-04-11 00:20:32 +0900

    ボタンのサイズを調整

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -294,7 +294,7 @@ export default function Projects() {
                 </>
               </CardFooter> */}
               <CardFooter>
-                <div className="flex flex-col gap-2">
+                <div className="flex flex-col gap-2 w-full">
                   <Button asChild className="w-full rounded-full group relative overflow-hidden px-32">
                     <motion.a
                       href={project.link}
